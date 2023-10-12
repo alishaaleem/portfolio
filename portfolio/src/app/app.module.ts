a034a7961bb90bf4779049b64d0900a3cdc258d3
Alisha Aleem <AAleem@imo-online.com>
2023-10-12 15:02:19 -0500

    added all the components required for portfolio

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { AcademicProjectsComponent } from './academic-projects/academic-projects.component';
+import { WhyHireMeComponent } from './why-hire-me/why-hire-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    CertificationsComponent,
+    AboutMeComponent,
+    ContactMeComponent,
+    AcademicProjectsComponent,
+    WhyHireMeComponent
   ],
   imports: [
     BrowserModule,
